d6d6916b81e0c69c67c18070f01fe34d04a510dc
Lukas Eipert <git@leipert.io>
2017-03-24 08:04:38 +0100

    update definitions

--- assets/definitions.js
+++ assets/definitions.js
@@ -1,37 +1,69 @@
 const definitions = [
-  { caskName: 'appcode', jetbrainsCode: 'AC', versionField: '<%= version %>' },
-  { caskName: 'clion', jetbrainsCode: 'CL', versionField: '<%= version %>' },
-  { caskName: 'datagrip', jetbrainsCode: 'DG', versionField: '<%= version %>' },
+  {
+    caskName: 'appcode',
+    jetbrainsCode: 'AC',
+    versionField: '<%= version %>,<%= build %>'
+  },
+  {
+    caskName: 'clion',
+    jetbrainsCode: 'CL',
+    versionField: '<%= version %>,<%= build %>'
+  },
+  {
+    caskName: 'datagrip',
+    jetbrainsCode: 'DG',
+    versionField: '<%= version %>,<%= build %>'
+  },
   {
     caskName: 'intellij-idea',
     jetbrainsCode: 'IIU',
-    versionField: '<%= version %>'
+    versionField: '<%= version %>,<%= build %>'
   },
   {
     caskName: 'intellij-idea-ce',
     jetbrainsCode: 'IIC',
-    versionField: '<%= version %>'
+    versionField: '<%= version %>,<%= build %>'
   },
   {
     caskName: 'jetbrains-toolbox',
     jetbrainsCode: 'TBA',
     versionField: '<%= version %>.<%= build %>'
   },
-  { caskName: 'mps', jetbrainsCode: 'MPS', versionField: '<%= version %>' },
-  { caskName: 'phpstorm', jetbrainsCode: 'PS', versionField: '<%= version %>' },
-  { caskName: 'pycharm', jetbrainsCode: 'PCP', versionField: '<%= version %>' },
+  {
+    caskName: 'mps',
+    jetbrainsCode: 'MPS',
+    versionField: '<%= version %>,<%= build %>'
+  },
+  {
+    caskName: 'phpstorm',
+    jetbrainsCode: 'PS',
+    versionField: '<%= version %>,<%= build %>'
+  },
+  {
+    caskName: 'pycharm',
+    jetbrainsCode: 'PCP',
+    versionField: '<%= version %>,<%= build %>'
+  },
   {
     caskName: 'pycharm-ce',
     jetbrainsCode: 'PCC',
-    versionField: '<%= version %>'
+    versionField: '<%= version %>,<%= build %>'
   },
   {
     caskName: 'pycharm-edu',
     jetbrainsCode: 'PCE',
-    versionField: '<%= version %>'
+    versionField: '<%= version %>,<%= build %>'
   },
-  { caskName: 'rubymine', jetbrainsCode: 'RM', versionField: '<%= version %>' },
-  { caskName: 'webstorm', jetbrainsCode: 'WS', versionField: '<%= version %>' },
+  {
+    caskName: 'rubymine',
+    jetbrainsCode: 'RM',
+    versionField: '<%= version %>,<%= build %>'
+  },
+  {
+    caskName: 'webstorm',
+    jetbrainsCode: 'WS',
+    versionField: '<%= version %>,<%= build %>'
+  },
   {
     caskName: 'youtrack-workflow',
     jetbrainsCode: 'YTWE',

c8cc78dddf1a1c2ab26a323d3c220a36e0da18ca
Lukas Eipert <git@leipert.io>
2018-05-27 12:57:38 +0200

    update example env file

--- env.example.sh
+++ env.example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export JCB_GITHUB_API_TOKEN="API_TOKEN"
-export JCB_TARGET_OWNER="leipert"
+export JCB_TARGET_OWNER="Homebrew"
 export JCB_TARGET_REPO="homebrew-cask"
 export JCB_SOURCE_FORK_OWNER="leipert"
 export JCB_PULLREQUEST_CC="/cc @leipert"

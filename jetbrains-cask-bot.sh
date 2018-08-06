62168f929d5cc5a701e4e08ef96ded3d18f914ba
Lukas Eipert <git@leipert.io>
2018-08-06 11:13:29 +0200

    add guard for missing env file

--- jetbrains-cask-bot.sh
+++ jetbrains-cask-bot.sh
@@ -5,6 +5,8 @@ set -euo pipefail; export FS=$'\n\t'
 # we save the current dir to go back to it later
 CURR_DIR="$(pwd)"
 
+CASK_DIR="$(brew --repository)/Library/Taps/homebrew/homebrew-cask"
+
 # we want to now where the project root is and switch to it
 __DIRNAME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -18,25 +20,29 @@ yarn
 brew update
 
 # read environment variables which contain git/github settings
-. "./env.sh" || echo "No env file found"
+if [[ -f "./env.sh" ]]; then
+    . "./env.sh"
+else
+    echo "No env file found"
+fi
 
 # Switch to cask tap and setup remote
-cd $(brew --repository)/Library/Taps/homebrew/homebrew-cask
+cd "${CASK_DIR}"
 git fetch --unshallow origin 2> /dev/null || echo "Repo already unshallow"
 git remote remove jcb 2> /dev/null || echo "No remote of the name jcb exists"
 git remote add jcb \
-    https://${JCB_GITHUB_API_TOKEN}@github.com/${JCB_SOURCE_FORK_OWNER}/${JCB_TARGET_REPO}.git > /dev/null 2>&1 \
+    "https://${JCB_GITHUB_API_TOKEN}@github.com/${JCB_SOURCE_FORK_OWNER}/${JCB_TARGET_REPO}.git" > /dev/null 2>&1 \
     && echo "Added jcb remote"
 
 # Switch to project root
 cd "${__DIRNAME}" || exit 1
 
 # run the script
-echo -e "\nRunning script\n"
+echo -e "\\nRunning script\\n"
 node "./lib/index.js"
 
 # Delete remote as it contains the API token
-cd $(brew --repository)/Library/Taps/homebrew/homebrew-cask
+cd "${CASK_DIR}"
 git remote remove jcb 2> /dev/null || echo "No remote of the name jcb exists"
 
 # go back to previous dir

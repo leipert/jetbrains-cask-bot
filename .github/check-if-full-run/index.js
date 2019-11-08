ed1a7db368629e9161e405b731638d78e36594e5
Lukas Eipert <git@leipert.io>
2019-11-08 01:54:10 +0100

    Update actions script

--- .github/check-if-full-run/index.js
+++ .github/check-if-full-run/index.js
@@ -7,115 +7,111 @@ const { allProducts } = require('./../../lib/shared');
 const { log, warn, error } = require('./../../lib/utils');
 const definitions = require('./../../assets/definitions');
 
-try {
-  core.setOutput('run_full', false);
+core.setOutput('run_full', 'no');
 
-  const PREV_FILE = path.join(__dirname, '..', '..', 'previous.json');
+const PREV_FILE = path.join(__dirname, '..', '..', 'previous.json');
 
-  const getPrevious = () => {
-    return fs.readJson(PREV_FILE).catch(err => err);
-  };
+const getPrevious = () => {
+  return fs.readJson(PREV_FILE).catch(err => err);
+};
 
-  const saveCurrent = currentData => {
-    return fs.writeJson(PREV_FILE, currentData).then(() => currentData);
-  };
+const saveCurrent = currentData => {
+  return fs.writeJson(PREV_FILE, currentData).then(() => currentData);
+};
 
-  let all = null;
+let all = null;
 
-  const getAllProducts = () => {
-    if (all) {
-      return Promise.resolve(all);
-    }
+const getAllProducts = () => {
+  if (all) {
+    return Promise.resolve(all);
+  }
 
-    return allProducts().then(products => {
-      all = products;
-      return products;
-    });
-  };
+  return allProducts().then(products => {
+    all = products;
+    return products;
+  });
+};
 
-  const getCurrent = () => {
-    return getAllProducts()
-      .then(allDefinitions => {
-        const jetbrainsCodes = definitions.map(x => x.jetbrainsCode);
-
-        return allDefinitions.filter(product => jetbrainsCodes.includes(product.code));
-      })
-      .then(saveCurrent)
-      .catch(err => err);
-  };
-
-  const lastReleaseDate = product =>
-    _.chain(product)
-      .get('releases', [])
-      .last()
-      .get('date', '1970-01-01')
-      .value();
-
-  const searchForUnknownProjects = () => {
-    return getAllProducts().then(allDefinitions => {
+const getCurrent = () => {
+  return getAllProducts()
+    .then(allDefinitions => {
       const jetbrainsCodes = definitions.map(x => x.jetbrainsCode);
 
-      const missingCasks = allDefinitions
-        .filter(
-          product =>
-            !jetbrainsCodes.includes(product.code) &&
-            _.get(product, 'distributions.mac', false) &&
-            lastReleaseDate(product) > '2018-12-11'
-        )
-        .map(product => {
-          const lastDownload = _.chain(product)
-            .get('releases', [])
-            .last()
-            .get('downloads.mac.link')
-            .value();
-          if (!lastDownload) {
-            return `${product.name} (${product.code}) has no downloadLink for MAC:
-          ${JSON.stringify(product, null, 2)}`;
-          }
-          return `Missing cask (${product.code}): ${product.name} (${product.link})
-    \t\tLast release: ${lastReleaseDate(product)}, ${lastDownload}`;
-        });
+      return allDefinitions.filter(product => jetbrainsCodes.includes(product.code));
+    })
+    .then(saveCurrent)
+    .catch(err => err);
+};
 
-      if (missingCasks.length > 0) {
-        throw new Error(`
+const lastReleaseDate = product =>
+  _.chain(product)
+    .get('releases', [])
+    .last()
+    .get('date', '1970-01-01')
+    .value();
+
+const searchForUnknownProjects = () => {
+  return getAllProducts().then(allDefinitions => {
+    const jetbrainsCodes = definitions.map(x => x.jetbrainsCode);
+
+    const missingCasks = allDefinitions
+      .filter(
+        product =>
+          !jetbrainsCodes.includes(product.code) &&
+          _.get(product, 'distributions.mac', false) &&
+          lastReleaseDate(product) > '2018-12-11'
+      )
+      .map(product => {
+        const lastDownload = _.chain(product)
+          .get('releases', [])
+          .last()
+          .get('downloads.mac.link')
+          .value();
+        if (!lastDownload) {
+          return `${product.name} (${product.code}) has no downloadLink for MAC:
+          ${JSON.stringify(product, null, 2)}`;
+        }
+        return `Missing cask (${product.code}): ${product.name} (${product.link})
+    \t\tLast release: ${lastReleaseDate(product)}, ${lastDownload}`;
+      });
+
+    if (missingCasks.length > 0) {
+      throw new Error(`
       Found unknown casks:
       \t${missingCasks.join('\n\t')}
     `);
-      }
-    });
-  };
+    }
+  });
+};
 
-  Promise.all([getPrevious(), getCurrent()])
-    .then(([previous, current]) => {
-      if (_.isError(current)) {
-        warn('Could not retrieve current jetbrains data');
-        return false;
-      }
+Promise.all([getPrevious(), getCurrent()])
+  .then(([previous, current]) => {
+    if (_.isError(current)) {
+      warn('Could not retrieve current jetbrains data');
+      return false;
+    }
 
-      if (_.isError(previous)) {
-        warn('Could not retrieve previous jetbrains data');
-        return true;
-      }
-
-      if (_.isEqual(previous, current)) {
-        warn('JetBrains has released no new versions');
-        return false;
-      }
-
+    if (_.isError(previous)) {
+      warn('Could not retrieve previous jetbrains data');
       return true;
-    })
-    .then(shouldTrigger => {
-      if (shouldTrigger) {
-        warn('Triggering build');
-        return core.setOutput('run_full', true);
-      }
-      log('A build was not triggered');
-    })
-    .then(() => searchForUnknownProjects())
-    .then(() => process.exit(0))
-    .catch(e => {
-      error(e.message);
-    });
-} catch (error) {
-  core.setFailed(error.message);
-}
+    }
+
+    if (_.isEqual(previous, current)) {
+      warn('JetBrains has released no new versions');
+      return false;
+    }
+
+    return true;
+  })
+  .then(shouldTrigger => {
+    if (shouldTrigger) {
+      warn('Triggering build');
+      return core.setOutput('run_full', 'yes');
+    }
+    log('A build was not triggered');
+  })
+  .then(() => searchForUnknownProjects())
+  .then(() => process.exit(0))
+  .catch(e => {
+    core.setFailed(e.message);
+  });
